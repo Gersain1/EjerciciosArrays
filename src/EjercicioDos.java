4deeb271149c88e19c5426e9b5149c75916533ae
Gersain1 <gersain.linares@hotmail.com>
2024-10-18 17:36:44 -0500

    ejercicidos

--- src/EjercicioDos.java
+++ src/EjercicioDos.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 
 public class EjercicioDos {
     public static void main(String[] args) {
-        ArrayList<>
+        //creación de arrays
+        ArrayList<Integer> numero = new ArrayList<>();
+
+        //bucle for
+        for (int i = 1; i<=5; i++){
+            numero.add(i);
+        }
+        System.out.println("numero" + numero);
     }
 }
